2778422f59145dce67c792d656dde69946e99c07
snekboss <62026503+snekboss@users.noreply.github.com>
2022-08-24 18:55:55 +0200

    Added the following things:
    - Added the PrefabManager.cs. It is a static class that statically loads Armor and Weapon prefabs (via lazy initialization).
    - Reorganized the folder structures in order to appease unity's resource manager "Resources".
    
    - I still need to test if things work, so this will be in a different branch for a bit. Once I've done a bit of local testing to see if it works, I'll merge it with the main branch. I won't commit the local testing, so this is probably *the* commit message you'll be seeing. If you're seeing this, then I must have been OK with my local tests (how ever accurate they might have been).

---
+++ JustBlade/Assets/Scripts/PrefabManager.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PrefabManager
+{
+    static List<Armor> headArmors;
+    public static List<Armor> HeadArmors 
+    {
+        get 
+        {
+            if (headArmors == null)
+            {
+                headArmors = LoadArmors("Armors/Head Armors");
+            }
+
+            return headArmors;
+        }
+    }
+
+    static List<Armor> torsoArmors;
+    public static List<Armor> TorsoArmors
+    {
+        get
+        {
+            if (torsoArmors == null)
+            {
+                torsoArmors = LoadArmors("Armors/Torso Armors");
+            }
+
+            return torsoArmors;
+        }
+    }
+
+    static List<Armor> handArmors;
+    public static List<Armor> HandArmors
+    {
+        get
+        {
+            if (handArmors == null)
+            {
+                handArmors = LoadArmors("Armors/Hand Armors");
+            }
+
+            return handArmors;
+        }
+    }
+
+    static List<Armor> legArmors;
+    public static List<Armor> LegArmors
+    {
+        get
+        {
+            if (legArmors == null)
+            {
+                legArmors = LoadArmors("Armors/Leg Armors");
+            }
+
+            return legArmors;
+        }
+    }
+
+    static List<Weapon> twoHandedWeapons;
+    public static List<Weapon> TwoHandedWeapons
+    {
+        get
+        {
+            if (twoHandedWeapons == null)
+            {
+                twoHandedWeapons = LoadWeapons("Weapons/Two Handed Weapons");
+            }
+
+            return twoHandedWeapons;
+        }
+    }
+
+    static List<Weapon> polearmWeapons;
+    public static List<Weapon> PolearmWeapons
+    {
+        get
+        {
+            if (polearmWeapons == null)
+            {
+                polearmWeapons = LoadWeapons("Weapons/Polearm Weapons");
+            }
+
+            return polearmWeapons;
+        }
+    }
+
+    static List<Armor> LoadArmors(string path)
+    {
+        List<Armor> ret = new List<Armor>();
+
+        UnityEngine.Object[] objs = Resources.LoadAll(path);
+        for (int i = 0; i < objs.Length; i++)
+        {
+            GameObject armorGO = objs[i] as GameObject;
+            Armor armor = armorGO.GetComponent<Armor>();
+            ret.Add(armor);
+        }
+
+        return ret;
+    }
+
+    static List<Weapon> LoadWeapons(string path)
+    {
+        List<Weapon> ret = new List<Weapon>();
+
+        UnityEngine.Object[] objs = Resources.LoadAll(path);
+        for (int i = 0; i < objs.Length; i++)
+        {
+            GameObject weaponGO = objs[i] as GameObject;
+            Weapon weapon = weaponGO.GetComponent<Weapon>();
+            ret.Add(weapon);
+        }
+
+        return ret;
+    }
+}
